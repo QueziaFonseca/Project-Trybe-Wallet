78b1da6c8c67a2e67db886ea77aae0b0e413b555
quezia-lima <qfonseca1@gmail.com>
2022-02-10 23:41:22 -0300

    Requisito 1:  configurando estruturas do Redux (Reducers)

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,26 @@
+import { GET_WALLET_DATA } from '../actions/walletAction';
+
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const initialState = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const wallet = (state = initialState, action) => {
+  switch (type) {
+  case GET_WALLET_DATA:
+    return {
+      ...state,
+      currencies: action.payload.currencies,
+      expenses: action.payload.expenses,
+
+    };
+
+  default:
+    return state;
+  }
+};
+
+export default wallet;
