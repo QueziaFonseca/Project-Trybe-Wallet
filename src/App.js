5a9e87bd3229da5ce0c4d809ea7e5696c6f0f659
quezia-lima <qfonseca1@gmail.com>
2022-02-11 13:29:36 -0300

    Requisito 1: Corrige App retirando <BrowserRouter>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/carteira" component={ Wallet } />
+        <Route exact path="/" component={ Login } />
+      </Switch>
+
+    );
+  }
 }
-
 export default App;
